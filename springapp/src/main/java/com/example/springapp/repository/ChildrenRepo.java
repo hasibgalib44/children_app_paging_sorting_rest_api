fac1d6c13db67bd26ef0c3877030bb5d8512cc15
neojarvis <techadmin@iamneo.ai>
2025-10-21 08:12:08 +0000

    Manual-save-commit-21/10/2025--13:42:07

--- springapp/src/main/java/com/example/springapp/repository/ChildrenRepo.java
+++ springapp/src/main/java/com/example/springapp/repository/ChildrenRepo.java
@@ -1,8 +1,8 @@
 package com.example.springapp.repository;
 import com.example.springapp.model.Children;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface ChildrenRepo extends JpaRepository<Children,Integer>{
+public interface ChildrenRepo extends PagingAndSortingRepository<Children,Integer>{
     
 }
