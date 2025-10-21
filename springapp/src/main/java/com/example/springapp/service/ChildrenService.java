c45a34e249fac1c69de370ca953ca2ae085ec94e
neojarvis <techadmin@iamneo.ai>
2025-10-21 05:41:00 +0000

    Auto-save-commit-21/10/2025--11:10:59

--- springapp/src/main/java/com/example/springapp/service/ChildrenService.java
+++ springapp/src/main/java/com/example/springapp/service/ChildrenService.java
@@ -3,8 +3,9 @@ import com.example.springapp.model.Children;
 import com.example.springapp.repository.ChildrenRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 @Service
 public class ChildrenService {
    @Autowired
@@ -14,9 +15,15 @@ public class ChildrenService {
     {
        chr.save(c);
     }
-    public List<Children> getChildrenSortedByField(String Field)
+    public List<Children> getChildrenSortedByField(String field)
     {
-        
+      return chr.findAll(Sort.by(Sort.Direction.ASC,field));
+    }
+
+
+    public List<Children> getChildrenOffsetWithPageSize(String field)
+    {
+      return chr.findAll(RequestPage.Of(offset,pagesize))
     }
     
 }
